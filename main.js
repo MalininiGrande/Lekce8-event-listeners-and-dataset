e41fd819eb993b9a21ba49cc5360ab78777f2d77
Sandra Malinova <90518039+MalininiGrande@users.noreply.github.com>
2022-04-11 20:27:30 +0200

    Obtain data from dataattributes

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log("test")
+
+let prvniOvoce = document.querySelector("#muj-div").dataset.ovoce;
+console.log(prvniOvoce);
+let druheOvoce = document.querySelector("#muj-druhy-div").dataset.ovoce;
+console.log(druheOvoce);
+
