9fd2b641f69c6c1b65386b01812f512129846a48
Sandra Malinova <90518039+MalininiGrande@users.noreply.github.com>
2022-04-11 21:21:51 +0200

    Arrow function

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 console.log("test")
 
 let prvniOvoce = document.querySelector("#muj-div").dataset.ovoce;
+// let prvniOvoce = document.getElementById("muj-div").dataset.ovoce; je to to same jako tradek nahore
+
 console.log(prvniOvoce);
 let druheOvoce = document.querySelector("#muj-druhy-div").dataset.ovoce;
+// let druheOvoce = document.getElementById("muj-druhy-div").dataset.ovoce; je to to same jako tradek nahore
+
 console.log(druheOvoce);
 
 let dataUrl = document.querySelector('body').dataset.dataUrl;
